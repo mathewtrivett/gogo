b0023d32db813adf241f94f212e00ea460bc0e46
Stuart Wood <stu.wood.94@googlemail.com>
2016-11-24 15:37:23 +0000

    Defined mergeGroup, isInGroup, and isCaptured

--- group.py
+++ group.py
@@ -1,8 +1,29 @@
-class Group:
-    def __init__(self, colour, coordinates, groupId):
-        self.colour = colour
-        self.coordinates = [coordinates]
-        self.id = groupID
+from Board import Board
 
-    def mergeGroup(self, group):
-        self.coordinates = self.coordinates + group.coordinates
+class Group:
+    
+    def __init__(self, colour, coordinates, groupId, isVirtual = False):
+        self.colour = colour
+        self.coordinates = {coordinates}
+        self.id = groupID
+        self.isVirtual = isVirtual#virtual groups are not on the game board and are used for analysis
+
+    def mergeGroup(self, group, board):
+        self.coordinates = self.coordinates.union(group.coordinates)
+        if(not self.isVirtual):#virtual
+            board.deleteGroup(group)
+
+    def isInGroup(self, testCoord):
+        for groupCoord in coordinates:
+            if(groupCoord == testCoord):
+                return True
+        return False
+
+    def isCaptured(self, board):
+        for coord in self.coordinates:
+            for neighbour in board.neighbours(coord):
+                if(neighbour isInGroup):#we must include this check so that the function can deal with virtual groups
+                    continue
+                elif board.getGroup(neighbour).colour == 0:#any empty square touching the group indicates it is not captured
+                    return False
+        return True
