7ffce7879699d3ee03c36a54702fe0bae92edff3
Stuart Wood <stu.wood.94@googlemail.com>
2016-11-24 15:57:31 +0000

    Added comments

--- group.py
+++ group.py
@@ -8,6 +8,10 @@ class Group:
         self.id = groupID
         self.isVirtual = isVirtual#virtual groups are not on the game board and are used for analysis
 
+    '''
+    Merges group into self, then telling the board to delete the reference to group,
+    if self is virtual then the board will remain unaffected
+    '''
     def mergeGroup(self, group, board):
         if(type(group.id) == NoneType):
             return
@@ -15,12 +19,19 @@ class Group:
         if(not self.isVirtual):#virtual
             board.deleteGroup(group)
 
+    '''
+    Checks is a given coordinate belongs to the group
+    '''
     def isInGroup(self, testCoord):
         for groupCoord in coordinates:
             if(groupCoord == testCoord):
                 return True
         return False
-
+    
+    '''
+    Checks if there is an empty square touching the group, if not then is it
+    captured and the function returns true
+    '''
     def isCaptured(self, board):
         for coord in self.coordinates:
             for neighbour in board.neighbours(coord):
