72c7db95dcb65d7f3272da87f8466cd256b307f1
Mathew Trivett <matt.trivett@gmail.com>
2016-11-24 15:54:49 +0000

    Added comments

--- board.py
+++ board.py
@@ -14,6 +14,12 @@ class Board:
     def isEmpty(self, coordinates):
         return self.matrix[coordinates[0]][coordinates[1]] == 0
 
+
+    '''
+    Neighbours looks at coordinates N, S, E and W of a given coordinate.
+    If it is not off the board it adds the coordinate to a set and returns the set.
+    '''
+
     def neighbours(self, coordinates):
         neighbours = set()
         for i in [(0,-1),(1,0),(0,1),(-1,0)]:
@@ -22,22 +28,35 @@ class Board:
                 neighbours.add(neighbour)
         return neighbours
 
+    '''
+    isSuicide takes a coordinate and a colour.
+    Creates a virtual group then checks all neighbours of the group
+    If it finds one with the same colour as itself, it merges that group
+    Then it checks if the group is captured, if true, then it is suicide.
+    '''
+
     def isSuicide(self,coordinates,colour):
         group = Group(coordinates, colour, isVirtual=True)
         for neighbour in self.neighbours(coordinates):
             if(neighbour.getGroup(coordinates).colour == group.colour):
-                pass
-        pass
-        # If encounters same colour stone
-        # Merge group
-        # Return is captured? on merged group
-
+                group.mergeGroup(neighbour)
+        return group.isCaptured(self)
+    
+    '''
+    Get group, takes a coordinate.  Looks through the groups list,
+    if its in a group returns the group.  If it is not returns None for id
+    and 0 for colour.
+    '''
+ 
     def getGroup(self, coordinates):
         for group in self.groups:
-            for coordinate in group.coordinates:
-                if(coordinates == coordinate):
-                    return group.id, group.colour
-        return None, 0
+            if group.isInGroup(coordinates):
+                return group
+        return { "id": None, "colour": 0 }
+
+    '''
+    isPlayable returns true if, onboard is true, isEmpty and isNotSuicide. 
+    '''
                     
     def isPlayable(self, coordinates):
         pass
