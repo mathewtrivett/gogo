65e6b20fe374fa6f9c3a4e12db36a01859ae60c7
Mathew Trivett <matt.trivett@gmail.com>
2016-11-26 00:06:19 +0000

    Weird merge statements in group.py

--- group.py
+++ group.py
@@ -1,18 +1,10 @@
 class Group:
     
-<<<<<<< HEAD
-    def __init__(self, colour, coordinates, groupId = 0, isVirtual = False):
-        self.coordinates = set()
-        self.colour = colour
-        self.coordinates = self.coordinates.union(coordinates)
-        self.id = groupId
-=======
     def __init__(self, colour, coordinates, groupId, isVirtual = False):
         self.coordinates = set()
         self.colour = colour
         self.coordinates = self.coordinates.union(coordinates)
         self.id = groupID
->>>>>>> group
         self.isVirtual = isVirtual #virtual groups are not on the game board and are used for analysis
 
     '''
