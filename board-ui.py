eaa6c934a62472e1de31f8ce903f37335ff3c311
Mathew Trivett <matt.trivett@gmail.com>
2016-11-28 16:36:26 +0000

    Got board working with different board sizes

--- board-ui.py
+++ board-ui.py
@@ -5,52 +5,90 @@ HEIGHT = 768
 FRAMERATE = 60
 BOARDCOLOUR = pygame.color.THECOLORS['chocolate4']
 BG = pygame.color.THECOLORS['linen']
+LINECOLOUR = pygame.color.THECOLORS['black']
+BOARD_SIZE = WIDTH * 0.60
+WHITESTONE = pygame.color.THECOLORS['linen']
+BLACKSTONE = pygame.color.THECOLORS['black']
 
-class Board():
-    
-    def __init__(self):
-        pass
+class Board_UI():
+    global screen
+    global WIDTH
+    global LINECOLOUR
+        
+    def __init__(self,grid, lineWidth=1):
+        self.grid = grid
+        self.divisor = self.grid + 1
+        self.bg = pygame.draw.rect(screen, BOARDCOLOUR, (WIDTH * 0.2, WIDTH* 0.05, BOARD_SIZE, BOARD_SIZE))
+        self.interval = self.bg.width / self.divisor # Divides the background by the grid size + 1
+        self.lineWidth = lineWidth # Sets the grid's line width
 
-    '''
-    Loop through a nested range up to the board size
-    For each, draw a line of a fixed length, offset by the correct amount.
-    '''
     def show(self):
-        global screen
-        BOARD_SIZE = WIDTH // 5 * 3
-        background = pygame.draw.rect(screen, BOARDCOLOR)
-        background.center
-        pass
+        # Board Background
+       
+        for position in range(self.grid+1):
+                if position == 0:
+                    continue
 
-class Button():
+                # Horizontal Lines
+                pygame.draw.line(screen,
+                             LINECOLOUR,
+                             (self.bg.x + self.interval, self.bg.y + self.interval * position),
+                             (self.bg.x + self.bg.width - self.interval, self.bg.y + self.interval * position),
+                             self.lineWidth)
+                
+                # Vertical Lines
+                pygame.draw.line(screen,
+                                 LINECOLOUR,
+                                 (self.bg.x + self.interval * position, self.bg.y + self.interval),
+                                 (self.bg.x + self.interval * position, self.bg.height + self.bg.y - self.interval),
+                                 self.lineWidth)
+
+                
+class Button_UI():
     pass
 
 
-class Timer():
+class Timer_UI():
     pass
 
 
-class Stone():
+class Stone_UI():
+
+    def __init__(self, Board_UI, colour, coordinates):
+        self.board = Board_UI
+        self.size = Board_UI.interval - 3
+        self.colour = colour
+        self.coordinates = coordinates
+
+    def show(self):
+        pygame.draw.circle(screen,
+                           self.colour,
+                           (int(self.board.bg.x + self.board.bg.width/2),int(self.board.bg.y + self.board.bg.height/2)),
+                           int(self.size/2))
+
+class Player_UI():
     pass
 
-
-class Player():
-    pass
-
-
-class CapturedStone(Stone):
+class CapturedStone_UI(Stone_UI):
     pass
 
 
 pygame.init()
-screen = pygame.display.set_mode([WIDTH,HEIGHT],pygame.RESIZABLE)
+screen = pygame.display.set_mode([WIDTH,HEIGHT])
 title = pygame.display.set_caption("GoGo")
 screen.fill(BG)
 pygame.display.flip()
 
-
 while True:
     event = pygame.event.poll()
+    board = Board_UI(5)
+    board.show()
+    black_stone = Stone_UI(board,BLACKSTONE,(0,0))
+    black_stone.show()
+    white_stone = Stone_UI(board,WHITESTONE,(3,3))
+    white_stone.show()
+    pygame.display.update()
+    
     if event.type == pygame.QUIT :
         break
 pygame.quit()
