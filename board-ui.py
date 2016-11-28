585d51f76b318d990deba8e20ccace89b16ffa76
Mathew Trivett <matt.trivett@gmail.com>
2016-11-28 16:56:36 +0000

    Just tidying up

--- board-ui.py
+++ board-ui.py
@@ -1,12 +1,15 @@
 import pygame
 
+## SIZE VARIABLES
 WIDTH = 1024
 HEIGHT = 768
 FRAMERATE = 60
-BOARDCOLOUR = pygame.color.THECOLORS['chocolate4']
-BG = pygame.color.THECOLORS['linen']
-LINECOLOUR = pygame.color.THECOLORS['black']
 BOARD_SIZE = WIDTH * 0.60
+
+## Colours and Textures
+BG = pygame.color.THECOLORS['linen']
+BOARDCOLOUR = pygame.color.THECOLORS['chocolate4']
+LINECOLOUR = pygame.color.THECOLORS['black']
 WHITESTONE = pygame.color.THECOLORS['linen']
 BLACKSTONE = pygame.color.THECOLORS['black']
 
@@ -15,7 +18,7 @@ class Board_UI():
     global WIDTH
     global LINECOLOUR
         
-    def __init__(self,grid, lineWidth=1):
+    def __init__(self, grid, lineWidth=1):
         self.grid = grid
         self.divisor = self.grid + 1
         self.bg = pygame.draw.rect(screen, BOARDCOLOUR, (WIDTH * 0.2, WIDTH* 0.05, BOARD_SIZE, BOARD_SIZE))
@@ -25,25 +28,25 @@ class Board_UI():
     def show(self):
         # Board Background
        
-        for position in range(self.grid+1):
+        for position in range(self.divisor):
                 if position == 0:
                     continue
 
-                # Horizontal Lines
+                # Horizontal Grid
                 pygame.draw.line(screen,
                              LINECOLOUR,
                              (self.bg.x + self.interval, self.bg.y + self.interval * position),
                              (self.bg.x + self.bg.width - self.interval, self.bg.y + self.interval * position),
                              self.lineWidth)
                 
-                # Vertical Lines
+                # Vertical Grid
                 pygame.draw.line(screen,
                                  LINECOLOUR,
                                  (self.bg.x + self.interval * position, self.bg.y + self.interval),
                                  (self.bg.x + self.interval * position, self.bg.height + self.bg.y - self.interval),
                                  self.lineWidth)
 
-                
+               
 class Button_UI():
     pass
 
@@ -56,7 +59,7 @@ class Stone_UI():
 
     def __init__(self, Board_UI, colour, coordinates):
         self.board = Board_UI
-        self.size = Board_UI.interval - 3
+        self.diameter = Board_UI.interval - 3
         self.colour = colour
         self.coordinates = coordinates
 
@@ -64,14 +67,13 @@ class Stone_UI():
         pygame.draw.circle(screen,
                            self.colour,
                            (int(self.board.bg.x + self.board.bg.width/2),int(self.board.bg.y + self.board.bg.height/2)),
-                           int(self.size/2))
+                           int(self.diameter/2))
 
 class Player_UI():
     pass
 
 class CapturedStone_UI(Stone_UI):
     pass
-
 
 pygame.init()
 screen = pygame.display.set_mode([WIDTH,HEIGHT])
@@ -81,7 +83,7 @@ pygame.display.flip()
 
 while True:
     event = pygame.event.poll()
-    board = Board_UI(5)
+    board = Board_UI(9)
     board.show()
     black_stone = Stone_UI(board,BLACKSTONE,(0,0))
     black_stone.show()
