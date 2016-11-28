fb143c49512223d9fd721b346ad3f3598cfc50e2
Stuart Wood <stu.wood.94@googlemail.com>
2016-11-28 15:35:06 +0000

    added passing and quitting

--- match.py
+++ match.py
@@ -8,25 +8,42 @@ class Match:
         self.currentBoard = self.board.getMatrix()
     
     '''
-    Evaulates one turn
+    Evaulates one turn, the return value indictates if the match should end
     '''
     def playTurn(self):
         stonePlaced = False
         while(not stonePlaced):
-            coordinate = tuple(map(int,input(self.players[self.currentPlayer].colour + " to play:").split(',')))
-            stonePlaced = self.players[self.currentPlayer].placeStone(self.board,coordinate,self.previousBoard)
-        
+            move = input(self.players[self.currentPlayer].colour + " to play:")
+            if move == "pass":
+                self.currentPlayer = (self.currentPlayer+1)%len(self.players)
+                return "passed"
+            elif move == "quit":
+                return "quit"
+            else:
+                coordinate = tuple(map(int,move.split(',')))
+                stonePlaced = self.players[self.currentPlayer].placeStone(self.board,coordinate,self.previousBoard)
         self.currentPlayer = (self.currentPlayer+1)%len(self.players)
         self.previousBoard = self.currentBoard
         self.currentBoard = self.board.getMatrix()
         print(self.board)
-        
+        return "placedStone"
+    
     '''
     The main loop of the match
     '''
     def matchLoop(self):
-        while(True):
-            self.playTurn()
-    
+        end = False
+        lastTurnPassed = False
+        while(not end):
+            move = self.playTurn()
+            if move == "passed":
+                if lastTurnPassed == True:
+                    end = True
+                lastTurnPassed = True
+            elif move == "quit":
+                end = True
+            elif move == "placedStone":
+                lastTurnPassed = False
+
 from board import Board
 from player import Player
