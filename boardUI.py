75c583e729186896dda0863ba4354d477dafbc3a
Mathew Trivett <matt.trivett@gmail.com>
2016-12-02 10:52:52 +0000

    Modified board

--- boardUI.py
+++ boardUI.py
@@ -1,7 +1,6 @@
 import pygame
 
 class BoardUI():
-    
     def __init__(self, grid, width, lineWidth, lineColour, boardColour,parent):
         self.grid = grid
         self.parent = parent
@@ -11,16 +10,21 @@ class BoardUI():
         self.boardColour = boardColour
 
         # Backboard
-        self.bg = pygame.draw.rect(self.parent, self.boardColour,
-                                   (self.parentWidth * 0.2, self.parentWidth * 0.05,
-                                    self.parentWidth * self.width, self.parentWidth * self.width))
+        self.image = pygame.Surface((self.parentWidth * self.width, self.parentWidth * self.width))
+        self.image.fill(self.boardColour)
+        self.parent.blit(self.image, (self.parentWidth * 0.2, self.parentWidth * 0.05))
+##        self.bg = pygame.draw.rect(self.parent, self.boardColour,
+##                                   (self.parentWidth * 0.2, self.parentWidth * 0.05,
+##                                    self.parentWidth * self.width, self.parentWidth * self.width))
 
+        self.rect = self.image.get_rect()
+        
         self.lineWidth = lineWidth # Sets the grid's line width
         self.lineColour = lineColour
 
         ## Dimensions
-        self.interval = self.bg.width / self.divisor # Divides the background by the divisor
-        self.origin = (self.bg.y + self.interval, self.bg.x + self.interval)
+        self.interval = self.image.get_width() / self.divisor # Divides the background by the divisor
+        self.origin = (self.rect.y + self.interval, self.rect.x + self.interval)
         self.farCorner = tuple(map(lambda x,y: x+self.interval*y, self.origin, (self.grid,self.grid)))
         self.coordinates = [[(x,y) for x in range(int(self.origin[0]), int(self.farCorner[0]), int(self.interval))] for y in range(int(self.origin[1]), int(self.farCorner[1]), int(self.interval))]
 
@@ -32,15 +36,15 @@ class BoardUI():
                 # Horizontal Grid
                 pygame.draw.line(self.parent,
                             self.lineColour,
-                             (self.bg.x + self.interval, self.bg.y + self.interval * position),
-                             (self.bg.x + self.bg.width - self.interval, self.bg.y + self.interval * position),
+                             (self.rect.x + self.interval, self.rect.y + self.interval * position),
+                             (self.rect.x + self.image.get_width() - self.interval, self.rect.y + self.interval * position),
                              self.lineWidth)
                 
                 # Vertical Grid
                 pygame.draw.line(self.parent,
                                 self.lineColour,
-                                 (self.bg.x + self.interval * position, self.bg.y + self.interval),
-                                 (self.bg.x + self.interval * position, self.bg.height + self.bg.y - self.interval),
+                                 (self.rect.x + self.interval * position, self.rect.y + self.interval),
+                                 (self.rect.x + self.interval * position, self.image.get_height() + self.rect.y - self.interval),
                                  self.lineWidth)
 
     def returnCoords(self, pixelCoords):
