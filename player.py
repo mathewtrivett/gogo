3ad20c2e4d59f7413572098b6c604144e4ff850f
Mathew Trivett <matt.trivett@gmail.com>
2016-12-08 14:20:50 +0000

    Now integrated some sound into gameplay

--- player.py
+++ player.py
@@ -30,10 +30,14 @@ class Player:
                 if newBoard.getGroup(neighbour).colour != 'None' and \
                     newBoard.getGroup(neighbour).colour != self.colour:
                         if newBoard.getGroup(neighbour).isCaptured(newBoard):
+                            captured_sound = UISound('./sounds/prisoners_captured/')
+                            captured_sound.update()
                             newPrisoners = newPrisoners + len(newBoard.getGroup(neighbour).coordinates)
                             newBoard.deleteGroup(newBoard.getGroup(neighbour))
             #check for ko
             if newBoard.getMatrix() != previousBoard:
+                place_stone_sound = UISound('./sounds/stone_placed/')
+                place_stone_sound.update()
                 currentBoard.setTo(newBoard)
                 self.prisoners = self.prisoners + newPrisoners
                 return True
@@ -48,3 +52,5 @@ class Player:
 from board import Board
 from copy import deepcopy
 from group import Group
+import pygame
+from ui_sound import UISound
