b5440b03d56feede2f1de2788e23e63510594d84
Stuart Wood <stu.wood.94@googlemail.com>
2016-12-08 14:54:27 +0000

    fixed keyboard input

--- inputs.py
+++ inputs.py
@@ -3,32 +3,24 @@ import pygame
 class Inputs():
     
     def __init__(self):
+        self.update()
         
-        pygame.init()
-
-        pygame.display.set_mode((100, 100))
-        while True:
-            keysPressed = pygame.key.get_pressed()
-            if keysPressed[pygame.K_a]:
-                print(a)
+    def update(self):
+        self.events = pygame.event.get()
         
     def keyIsDown(self,keyCode):
         keysPressed = pygame.key.get_pressed()
         return keysPressed[keyCode]
     
     def keyWasPressed(self,keyCode):
-        events = pygame.event.get()
-        for event in events:
+        for event in self.events:
             if event.type == pygame.KEYDOWN:
                 if event.key == keyCode:
                     return True
         return False
     
     def hasQuit(self):
-        events = pygame.event.get()
-        for event in events:
+        for event in self.events:
             if event.type == pygame.QUIT:
                 return True
         return False
-    
-i = Inputs()
